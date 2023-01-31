78d2963d7ea2f13c8f67be5cc118c731ad0e669a
phdljr <phdljr@naver.com>
2023-01-31 22:50:13 +0900

    feat: Add Password encoder

--- src/main/java/kr/ac/phdljr/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/kr/ac/phdljr/springbootsecurity/config/SecurityConfig.java
@@ -3,14 +3,21 @@ package kr.ac.phdljr.springbootsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 public class SecurityConfig {
+
+    // 비밀번호 암호화 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -18,12 +25,16 @@ public class SecurityConfig {
                 .csrf().disable()
 
                 .authorizeRequests()
-                // 로그인 한 사람만 들어올 수 있음
+                // 로그인 한 사람만 들어올 수 있음(인증)
                 .antMatchers("/user/**").authenticated()
-                // 권한이 있는 사람만 접근 가능
+                // 권한이 있는 사람만 접근 가능(권한)
                 .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll();
+                // 나머지 주소는 모든 사용자가 접근 가능
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm");
         return http.build();
     }
 
--- src/main/java/kr/ac/phdljr/springbootsecurity/controller/IndexController.java
+++ src/main/java/kr/ac/phdljr/springbootsecurity/controller/IndexController.java
@@ -1,10 +1,20 @@
 package kr.ac.phdljr.springbootsecurity.controller;
 
+import kr.ac.phdljr.springbootsecurity.model.User;
+import kr.ac.phdljr.springbootsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping("/")
     public String index(){
         return "Hello!";
@@ -25,18 +35,29 @@ public class IndexController {
         return "manager";
     }
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
     }
 
     @GetMapping("/joinProc")
     public String joinProc(){
         return "회원가입 완료!";
+    }
+
+    @PostMapping ("/join")
+    public String join(User user){
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+
+        userRepository.save(user); // 비밀번호가 그대로 저장됨 -> 시큐리티로 로그인을 할 수 없음(암호화가 안되어 있어서)
+        return "redirect:/loginForm";
     }
 }
