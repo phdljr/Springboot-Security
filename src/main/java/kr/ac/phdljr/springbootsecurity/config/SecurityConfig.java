a1cd80abbf83ad4a5b6bf6787bd2283ce6ff25f5
phdljr <phdljr@naver.com>
2023-01-31 15:30:07 +0900

    feat: Spring Security 적용

--- src/main/java/kr/ac/phdljr/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/kr/ac/phdljr/springbootsecurity/config/SecurityConfig.java
@@ -1,9 +1,34 @@
 package kr.ac.phdljr.springbootsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Cross Site Request Forgery 사이트간 위조 요청
+                .csrf().disable()
+
+                .authorizeRequests()
+                // 로그인 한 사람만 들어올 수 있음
+                .antMatchers("/user/**").authenticated()
+                // 권한이 있는 사람만 접근 가능
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/resources/**");
+    }
 }
